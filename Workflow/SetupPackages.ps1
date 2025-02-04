823fae37adc679ef66627f2d36fff35a52a541ef
Jiri Formacek <jiri.formacek@greycorbel.com>
2025-02-04 13:19:25 +0100

    updated action

--- Workflow/SetupPackages.ps1
+++ Workflow/SetupPackages.ps1
@@ -3,15 +3,16 @@ using namespace System.IO
 param
 (
  [string]$ModuleName,
- [string]$RootPath,
- [string]$nugetPath
+ [string]$RootPath
 )
 
-$packagesDir = [Path]::Combine($RootPath,'packages')
+$packagesDir = [Path]::Combine($env:UserProfile,'.nuget','packages')
 $modulePath = [Path]::Combine($RootPath,'Module',$ModuleName)
 $sharedPath = [Path]::Combine($modulePath,'lib')
 
-&"$nugetPath" restore ([Path]::Combine($RootPath,'Workflow','packages.config')) -packagesDirectory $packagesDir | Out-Null
+<#
+    &"$nugetPath" restore ([Path]::Combine($RootPath,'Workflow','packages.config')) -packagesDirectory $packagesDir | Out-Null
+#>
 "Updating packages in the module"
 $packages = ([xml](get-content -path ([Path]::Combine($RootPath,'Workflow','packages.config')) -raw)).packages.package
 $packages
@@ -20,5 +21,5 @@ if(-not (Test-Path -Path ([Path]::Combine($sharedPath, 'net8.0')))) { New-Item -
 
 $pkg = $packages | where-object{$_.id -eq "DnsClient"}
 "Processing: $($pkg.id)"
-$packageFolder = [Path]::Combine($packagesDir, "$($pkg.id)`.$($pkg.version)")
+$packageFolder = [Path]::Combine($packagesDir, $pkg.id, $pkg.version)
 Copy-Item -Path ([Path]::Combine($packageFolder,'lib','net8.0',"$($pkg.id)`.dll")) -Destination ([Path]::Combine($sharedPath,'net8.0')) -Force
